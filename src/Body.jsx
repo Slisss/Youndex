4556da17a22ab8288f3c11aeda9e400e0dc72b93
Slisss <144116661+Slisss@users.noreply.github.com>
2024-12-03 03:20:49 +0300

    Изменил структуру

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,29 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import Login from './Login';
-import Post from './Post';
 import Profile from './Profile';
-import NewPost from './NewPost';
+import Login from './Login';
+import Navigation from './Navigation';
 
 function Body() {
 	let [userOnPage, setUserOnPage] = useState('');
 	let [loggedIn, setLoggedIn] = useState(0);
 
-	let [allPosts, setPosts] = useState([]);
-	let [postCount, setPostCount] = useState(0);
-
 	let [profileOwner, setProfileOwner] = useState('');
-	
 	let dictionary = ['qawsed', 'zxc'];
 	
-	useEffect(() => {
-		fetch('http://127.0.0.1:8000/api/Postlist')
-		.then(response => response.json())
-		.then(posts => {
-			setPosts(posts);
-			setPostCount(posts.length);
-		})
-	}, [postCount]);
-
 	function ShowProfile(username) {
 		setProfileOwner(username);
 		document.querySelector('div.profile').style.display = 'block';
@@ -48,30 +34,17 @@ function Body() {
 					<button id='profile' onClick={() => {ShowProfile(userOnPage)}}>Show Your Profile</button>
 				</div>
 			: ''}
-			{loggedIn ? allPosts.map((p) =>
-				<Post
-					post={p}
-					userOnPage={userOnPage}
-					postCount={postCount}
-					setPostCount={setPostCount}
-					setProfileOwner={setProfileOwner}
-					ShowProfile={ShowProfile}
-					dict={dictionary}
-					CheckDict={CheckDict}
-				/>
-			) : ''}
-			{loggedIn ? <Profile
+			<Profile
 				userOnPage={userOnPage}
 				profileOwner={profileOwner}
 				dict={dictionary}
 				CheckDict={CheckDict}
 			/>
-			: ''}
 			{loggedIn ?
-				<NewPost
+				<Navigation
 					userOnPage={userOnPage}
-					postCount={postCount}
-					setPostCount={setPostCount}
+					setProfileOwner={setProfileOwner}
+					ShowProfile={ShowProfile}
 					dict={dictionary}
 					CheckDict={CheckDict}
 				/>
