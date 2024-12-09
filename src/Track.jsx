b4f2389206d1237fb0cb184113654b25e4bcb39c
highscroller <137916218+highscroller@users.noreply.github.com>
2024-12-09 23:40:09 +0300

    добавил функционал музыки

--- src/Track.jsx
+++ src/Track.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-function Track({track}) {
+function Track({track, play}) {
 	
 	return (
 		<div id={'t' + track.id} className='track'>
 			<p>{track.author}</p>
 			<p>{track.name}</p>
 			<p>{track.album}</p>
-			<button>Play</button>
+			<button onClick={() => {play(track)}}>Play</button>
 		</div>
 	);
 }
