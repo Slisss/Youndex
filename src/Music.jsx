48b386b2c3fd09057b86908b93af6e639b1c83b7
highscroller <137916218+highscroller@users.noreply.github.com>
2024-12-17 10:59:17 +0300

    Обновлен функционал музыки

--- src/Music.jsx
+++ src/Music.jsx
@@ -1,16 +1,68 @@
 import React, { useState, useEffect } from 'react';
 import Track from './Track';
 function Music() {
-	let [trax, setTrax] = useState([
-		{id: 1, author: 'The Prodigy', name: 'Firestarter', album: 'The Fat Of The Land', src: 'https://www.dropbox.com/scl/fi/7rno5hx2ywub7fcec194c/08.-Firestarter.mp3?rlkey=4ybtjby5tmm0cmf1iev5ms7un&st=ip6ryctb&raw=1'},
-		{id: 2, author: 'Junkie XL', name: 'Underachievers', album: 'Saturday Teenage Kick', src: 'https://www.dropbox.com/scl/fi/xltnm2ov05gz7xeb5kqn0/12.-Junkie-XL-Underachievers.mp3?rlkey=7plotjf1wy8wrvazkjhd96he6&st=ouvdobwn&raw=1'},
-		{id: 3, author: 'Netsky', name: 'Iron Heart', album: 'Netsky', src: 'https://www.dropbox.com/scl/fi/t9ve7a4ucbajd25aj7nhg/02-Netsky-Iron-Heart.mp3?rlkey=nu95comxudy790s5upmllejii&st=wuxsc1gr&raw=1'},
-	]);
-	let [curTrack, setCT] = useState(trax[0]);
+	let [allTrax, setTrax] = useState([]);
+	let [traxCount, setTraxCount] = useState(0);
 	
+	let [curTrack, setCT] = useState(0);
+	
+	let [newTrackId, setNewTrackId] = useState(0);
+	
+	useEffect(() => {
+		fetch('http://127.0.0.1:8000/api/Trackcount')
+		.then(res => res.json())
+		.then(res => setNewTrackId(res.id__max+1))
+	}, [traxCount])
+	
+	useEffect(() => {
+		fetch('http://127.0.0.1:8000/api/Tracklist')
+		.then(response => response.json())
+		.then(trax => {
+			setTrax(trax.sort((a, b) => b.likes - a.likes));
+			setTraxCount(trax.length);
+		})
+	}, [traxCount]);
+	 function OpenForm() {
+		document.querySelector('form.newTrack').style.display = 'block';
+	 }
+	function CloseForm(e) {
+		e.preventDefault();
+		document.querySelector('form.newTrack').style.display = 'none';
+		document.querySelector('form.newTrack > #author').value = '';
+		document.querySelector('form.newTrack > #name').value = '';
+		document.querySelector('form.newTrack > #album').value = '';
+		document.querySelector('form.newTrack > #src').value = '';
+	}
+	function Upload(e) {
+		e.preventDefault();
+		
+		let in1 = document.querySelector('form.newTrack > #author').value;
+		let in2 = document.querySelector('form.newTrack > #name').value;
+		let in3 = document.querySelector('form.newTrack > #album').value;
+		let in4 = document.querySelector('form.newTrack > #src').value;
+		
+		if (in1 == '' || in2 == '' || in3 == '' || in4 == '') { window.alert('all fields must not be empty'); return; }
+		if (in1.length > 200 || in2.length > 200 || in3.length > 200 || in4.length > 300)  { window.alert('some fields exceed allowed length'); return; }
+		
+		in4.length -= 4;
+		in4 += 'raw=1';
+		
+		fetch('http://127.0.0.1:8000/api/Tracklist', { 
+			method: 'POST',
+			body: JSON.stringify({
+				id: newTrackId,
+				author: in1,
+				name: in2,
+				album: in3,
+				src: in4
+			}),
+			headers: { 'Content-type': 'application/json; charset=UTF-8' }
+		})
+		.then(res => { setTrackCount(traxCount + 1); CloseForm(e); })
+	}
 	function MoveTrack(incr) {
-		let fincr = (trax.indexOf(curTrack) + incr < 0) ? trax.length - 1 : (trax.indexOf(curTrack) + incr >= trax.length) ? 0 : trax.indexOf(curTrack) + incr;
-		setCT(trax[fincr]);
+		let fincr = (allTrax.indexOf(curTrack) + incr < 0) ? allTrax.length - 1 : (allTrax.indexOf(curTrack) + incr >= allTrax.length) ? 0 : allTrax.indexOf(curTrack) + incr;
+		setCT(allTrax[fincr]);
 		setTimeout(() => { document.querySelector('audio').play(); }, 1);
 	}
 	function PlayTrack(t) {
@@ -19,21 +71,45 @@ function Music() {
 	}
 	return (
 		<div className='music'>
-			{trax.map((t) => 
+			{allTrax.map((t) => 
 				<Track
+					key={t.id}
 					track={t}
 					play={PlayTrack}
+					traxCount={traxCount}
+					setTraxCount={setTraxCount}
 				/>
 			)}
+			
+			<button className='addNew' onClick={OpenForm}>Add New Song</button>
+			
 			<div className='curTrack'>
-				<p>Now Playing:</p>
-				<audio controls src={curTrack.src} type='audio/mpeg' onEnded={()=>{MoveTrack(1)}} />
-				<p>{curTrack.author}</p>
-				<p>{curTrack.name}</p>
-				<p>{curTrack.album}</p>
+				<div className='title'>Now Playing:</div>
+				<div className='trackData'>
+					<p>{curTrack.author}</p>
+					<p>{curTrack.name}</p>
+					<p>{curTrack.album}</p>
+				</div>
+				<audio controls controlsList='nodownload' src={curTrack.src} type='audio/mpeg' onEnded={()=>{MoveTrack(1)}} />
 				<button onClick={()=>{MoveTrack(-1)}}>Prev</button>
 				<button onClick={()=>{MoveTrack( 1)}}>Next</button>
 			</div>
+			
+			<form className='newTrack'>
+				<h1>Upload a New Track</h1>
+				<label>Author</label><br/>
+				<input id='author' max='200'></input><br/>
+				<label>Song Name</label><br/>
+				<input id='name' max='200'></input><br/>
+				<label>Album</label><br/>
+				<input id='album' max='200'></input><br/>
+				<label>Valid Dropbox Link</label><br/>
+				<input id='src' max='300'></input>
+				<div className='controls'>
+					<button onClick={Upload}>Upload</button>
+					<button onClick={CloseForm}>Cancel</button>
+				</div>
+			</form>
 		</div>
 	);
 }
--- src/Track.jsx
+++ src/Track.jsx
@@ -1,12 +1,49 @@
 import React, { useState, useEffect } from 'react';
-function Track({track, play}) {
+function Track({track, play, traxCount, setTraxCount}) {
 	
+	let [l, setL] = useState(false);
+	let [d, setD] = useState(false);
+	
+	function Like(incr) {
+		if (incr > 0) {
+			if (l) { incr *= -1; setL(false); }
+			else { if (d) incr *= 2; setL(true); setD(false); }
+		}
+		else {
+			if (d) { incr *= -1; setD(false); }
+			else { if (l) incr *= 2; setD(true); setL(false); }
+		}
+		
+		fetch(`http://127.0.0.1:8000/api/Tracklist/${track.id}`, {
+			method: 'PUT',
+			body: JSON.stringify({
+				id: track.id,
+				author: track.author,
+				name: track.name,
+				album: track.album,
+				src: track.src,
+				likes: track.likes + incr
+			}),
+			headers: { 'Content-type': 'application/json; charset=UTF-8' }
+		})
+		.then(res => {
+			setTraxCount(traxCount - 1);
+			setTraxCount(traxCount + 1);
+		})
+	}
 	return (
 		<div id={'t' + track.id} className='track'>
-			<p>{track.author}</p>
-			<p>{track.name}</p>
-			<p>{track.album}</p>
-			<button onClick={() => {play(track)}}>Play</button>
+			<div className='trackData'>
+				<p>{track.author}</p>
+				<p>{track.name}</p>
+				<p>{track.album}</p>
+			</div>
+			<div className='likes'>
+				<button id='l' onClick={() => {Like( 1)}}>+</button>
+				<div>{track.likes}</div>
+				<button id='d' onClick={() => {Like(-1)}}>-</button>
+			</div>
+			<button id='play' onClick={() => {play(track)}}>Play</button>
 		</div>
 	);
 }
